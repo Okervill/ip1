242221de51f904ae3aeeaf7a50aa9aca5b5d7ae5
Andrew Reid <47385367+Andarooreed@users.noreply.github.com>
2019-02-15 12:00:08 +0000

    animation package
    created an animation package and moved shaker to this.

--- IntegratedProject1/src/Animation/Shaker.java
+++ IntegratedProject1/src/Animation/Shaker.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Login;
+package Animation;
 import javafx.animation.TranslateTransition;
 import javafx.scene.Node;
 import javafx.util.Duration;
